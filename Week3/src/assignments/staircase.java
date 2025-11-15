062cfe0e805dc6c07fcf429664da54f8c966d9a3
imran-rabi <kidpalestine2023@gmail.com>
2025-11-15 12:39:07 -0600

    Staircase assignment complete

--- Week3/src/assignments/staircase.java
+++ Week3/src/assignments/staircase.java
@@ -1,4 +1,27 @@
 package assignments;
 
 public class staircase {
+    public static void main (String... args){
+
+
+        int N = Integer.parseInt(args[0]);
+
+        for(int i = 1; i <= N; i++){
+
+            for(int n = 1; n <= i; n++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for(int i = N - 1; i >= 1; i--){
+
+            for(int n = 1; n <= i; n++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
 }
